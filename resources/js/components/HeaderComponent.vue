8fc316d1abd13b2767e4bcc8a35a2d2ba6891e3a
mwleinad <dlopez@trazzos.com>
2019-11-28 14:39:47 -0600

    created some default components to build stuff more easy

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-app-bar
+        :clipped-left="primaryDrawer.clipped"
+        app
+    >
+        <v-app-bar-nav-icon
+            @click.stop="primaryDrawer.model = !primaryDrawer.model"
+        />
+        <v-toolbar-title>Comprobante Digital</v-toolbar-title>
+    </v-app-bar>
+</template>
+
+
+<script>
+  export default {
+    data: () => ({
+      primaryDrawer: {
+        model: null,
+        clipped: true,
+        mini: false,
+      },
+    }),
+  }
+</script>
---
+++ resources/js/components/NavigationComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-navigation-drawer
+        v-model="primaryDrawer.model"
+        :clipped="primaryDrawer.clipped"
+        :mini-variant="primaryDrawer.mini"
+        app
+        overflow
+    >
+        <v-list dense>
+            <v-list-item link :to="{ name: 'home' }">
+                <v-list-item-action>
+                    <v-icon>mdi-home</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title>Inicio</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item link :to="{ name: 'hello' }">
+                <v-list-item-action>
+                    <v-icon>mdi-contact-mail</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title>Empresa</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+    //TODO next use a vuex store to handle these values :P
+  export default {
+    data: () => ({
+      primaryDrawer: {
+        model: null,
+        clipped: true,
+        mini: false,
+      },
+    }),
+  }
+</script>
