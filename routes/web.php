adc44bf39c8c6964b8f31519492c78600485e9e8
mwleinad <dlopez@trazzos.com>
2019-11-28 13:35:09 -0600

    added module back

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,6 @@
 |
 */
 
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
-
-Route::get('/{any}', 'SpaController')->where('any', '.*');
+Route::get('/{any}', 'SpaController')
+    ->where('any', '^(?!api).*$')
+    ->name('administration');
