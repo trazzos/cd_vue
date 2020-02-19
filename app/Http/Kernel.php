a1f8ff3f3ced37aa5281c5381287176d195d61bc
Hector <isc061990@gmail.com>
2020-02-19 13:27:50 -0600

    change to kernel to allow preflight requests

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,6 +20,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        Cors::class,
     ];
 
     /**
@@ -41,7 +42,6 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
-            Cors::class,
         ],
     ];
 
