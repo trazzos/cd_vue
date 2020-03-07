0675d981aa0502375fc17b0778e8cb07e12ac4d2
loranca <lorancaazpe@gmail.com>
2020-03-07 13:35:48 -0600

    preproject

---
+++ database/migrations/2020_03_05_233523_create_year_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateYearTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('year', function (Blueprint $table) {
+            $table->integer('year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('year');
+    }
+}
