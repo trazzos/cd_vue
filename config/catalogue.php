916ba3db6b3a90e1ba7aaede686fc261dd3acb80
Hector <isc061990@gmail.com>
2020-03-12 13:33:11 -0600

    archivo catalogue.php

---
+++ config/catalogue.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'award_type'=>[
+        '0' => [
+            'id'=>1,
+            'name' => 'Asignación directa'
+        ],
+        '1' => [
+            'id'=>2,
+            'name' => 'Invitación restringida'
+        ],
+        '2' => [
+            'id'=>3,
+            'name' => 'Licitación'
+        ],
+    ],
+
+];
